06b4e77b546b8a2abb8f059fe62032d469d2d899
Kutty Kutty <perumalpravinraj@gmail.com>
2024-07-29 21:49:18 +0530

    Added photo

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { techStack } from "../constants";
 import { ThemeContext } from "../themeProvider";
 import { motion } from "framer-motion";
+import pravinImage from "../assets/pravin.jpg";
 
 const About = () => {
   const theme = useContext(ThemeContext);
@@ -19,32 +20,41 @@ const About = () => {
           About Me
         </h2>
         <div>
-          <motion.div>
-            <h4 className="mt-12 text-3xl font-semibold text-blue-500">
-              A bit about me
-            </h4>
-            <p
-              className={
-                darkMode
-                  ? "mt-4 text-xl text-justify text-gray-500"
-                  : "mt-4 text-xl text-justify text-white"
-              }
-            >
-              I’m an aspiring Artificial Intelligence Engineer with a 
-              profound passion for data science and a comprehensive 
-              skill set in cloud computing and full stack development. 
-              My academic journey has involved developing advanced AI 
-              models and working on projects that integrate both software 
-              and hardware solutions. I've gained experience in designing 
-              scalable cloud architectures and building end-to-end applications, 
-              enhancing my ability to address complex challenges. 
-              These experiences have honed my problem-solving skills 
-              and fueled my enthusiasm for innovative technology. 
-              With a commitment to continuous learning and a drive 
-              for excellence, I am eager to contribute to dynamic 
-              teams and make a meaningful impact.
-            </p>
-          </motion.div>
+          <div className="flex flex-col md:flex-row gap-4 mt-12 px-4 sm:px-6 md:px-0">
+            <motion.div className="md:w-4/5 order-2 md:order-1">
+              <h4 className="text-3xl font-semibold text-blue-500">A bit about me</h4>
+              <p
+                className={
+                  darkMode
+                    ? "mt-4 text-xl text-justify text-gray-500"
+                    : "mt-4 text-xl text-justify text-white"
+                }
+              >
+                I’m an aspiring Artificial Intelligence Engineer with a
+                profound passion for data science and a comprehensive
+                skill set in cloud computing and full stack development.
+                My academic journey has involved developing advanced AI
+                models and working on projects that integrate both software
+                and hardware solutions. I've gained experience in designing
+                scalable cloud architectures and building end-to-end applications,
+                enhancing my ability to address complex challenges.
+                These experiences have honed my problem-solving skills
+                and fueled my enthusiasm for innovative technology.
+                With a commitment to continuous learning and a drive
+                for excellence, I am eager to contribute to dynamic
+                teams and make a meaningful impact.
+              </p>
+            </motion.div>
+            <motion.div className="md:w-1/5 order-1 md:order-2 flex justify-center items-center mt-8 md:mt-0">
+              <img
+                src={pravinImage}
+                alt="Pravin Raj"
+                className="w-full h-auto max-w-full max-h-80 object-contain rounded-lg"
+              />
+            </motion.div>
+
+          </div>
+
           <motion.div
           >
             <h4 className="mt-12 text-3xl font-semibold text-blue-500">
@@ -57,9 +67,9 @@ const About = () => {
                   : "mt-4 text-xl text-justify text-white"
               }
             >
-              Leveraging a combination of advanced AI techniques and data science methodologies, 
-              I build intelligent systems and solutions. 
-              Additionally, my expertise in cloud computing and 
+              Leveraging a combination of advanced AI techniques and data science methodologies,
+              I build intelligent systems and solutions.
+              Additionally, my expertise in cloud computing and
               full stack development enables me to create scalable, high-performance applications
             </p>
           </motion.div>
@@ -87,7 +97,7 @@ const About = () => {
           </motion.div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
