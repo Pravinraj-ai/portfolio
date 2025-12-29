c583e78d6094db18e62987423f91c6c7adeee3e2
PRAVIN RAJ <perumalpravinraj@gmail.com>
2025-12-30 00:35:41 +0530

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -96,7 +96,7 @@ const Home = () => {
       {showResume && (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50">
           <iframe
-            src="https://drive.google.com/file/d/1SMu-eOWIyXGI9Rw2lA2Yg2yHXHsMIHcc/view?usp=sharing"
+            src="https://drive.google.com/file/d/1SMu-eOWIyXGI9Rw2lA2Yg2yHXHsMIHcc/view?usp=drivesdk"
             width="80%"
             height="80%"
             frameBorder="0"
